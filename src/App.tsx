6f1470e9d40f12ccdac185faa34d5566084aea39
Hesham <hesham.fouad195@gmail.com>
2022-12-02 19:56:53 +0500

    app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import './index.css'
 
 function App() {
  
 
   return (
-    <div className="App">
+    <div className="text-blue-600 text-3xl font-bold">
      <h1>Test App</h1>
     </div>
   )
